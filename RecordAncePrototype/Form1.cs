a7dd2c487fed187af1455926d2d0de917c72d908
andresarazaru <johnandreisalazar0@gmail.com>
2024-05-29 14:15:22 +0800

    Add project files.

---
+++ RecordAncePrototype/Form1.cs
@@ -0,0 +1,20 @@
+namespace RecordAncePrototype
+{
+    public partial class SignInPage : Form
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void signinButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void signupLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
